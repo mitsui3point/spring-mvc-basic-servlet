633d8950e8c5aba07c9fa46bcd59440f702842c8
mitsui3point <rladbsgh0331@gmail.com>
2023-01-12 13:14:45 +0900

    feat($web.frontcontroller,$test.web.frontcontroller): add - add: 뷰 리졸버 : 이번에는 뷰 리졸버에 대해서 자세히 알아보자.
    - OldController - View 조회할 수 있도록 변경
    : View를 사용할 수 있도록 다음 코드를 추가했다.
    : return new ModelAndView("new-form");
    
    - 실행
    : http://localhost:8080/springmvc/old-controller
    : 웹 브라우저에 Whitelabel Error Page 가 나오고, 콘솔에 OldController.handleRequest 이 출력될 것이다.
    
    - 실행해보면 컨트롤러를 정상 호출되지만, Whitelabel Error Page 오류가 발생한다.
    : application.properties 에 다음 코드를 추가하자
    
    application.properties```
    spring.mvc.view.prefix=/WEB-INF/views/
    spring.mvc.view.suffix=.jsp
    ```
    
    - 뷰 리졸버 - InternalResourceViewResolver
    : 스프링 부트는 InternalResourceViewResolver 라는 뷰 리졸버를 자동으로 등록하는데, 이때 application.properties 에 등록한 spring.mvc.view.prefix , spring.mvc.view.suffix 설정 정보를 사용해서 등록한다.
    : 참고로 권장하지는 않지만 설정 없이 다음과 같이 전체 경로를 주어도 동작하기는 한다.
    : return new ModelAndView("/WEB-INF/views/new-form.jsp");
    
    - 실행
    : http://localhost:8080/springmvc/old-controller
    :등록 폼이 정상 출력되는 것을 확인할 수 있다. 물론 저장 기능을 개발하지 않았으므로 폼만 출력되고, 더 진행하면 오류가 발생한다.
    
    <뷰 리졸버 동작 방식>
    - 스프링 부트가 자동 등록하는 뷰 리졸버
    : (실제로는 더 많지만, 중요한 부분 위주로 설명하기 위해 일부 생략)
    1 = BeanNameViewResolver : 빈 이름으로 뷰를 찾아서 반환한다. (예: 엑셀 파일 생성 기능에 사용)
    2 = InternalResourceViewResolver : JSP를 처리할 수 있는 뷰를 반환한다.
    
    1. 핸들러 어댑터 호출
    : 핸들러 어댑터를 통해 new-form 이라는 논리 뷰 이름을 획득한다.
    
    2. ViewResolver 호출
    : new-form 이라는 뷰 이름으로 viewResolver를 순서대로 호출한다.
    : BeanNameViewResolver 는 new-form 이라는 이름의 스프링 빈으로 등록된 뷰를 찾아야 하는데 없다.
    : InternalResourceViewResolver 가 호출된다.
    
    3. InternalResourceViewResolver
    : 이 뷰 리졸버는 InternalResourceView 를 반환한다.
    
    4. 뷰 - InternalResourceView
    : InternalResourceView 는 JSP처럼 포워드 forward() 를 호출해서 처리할 수 있는 경우에 사용한다.
    
    5. view.render()
    : view.render() 가 호출되고 InternalResourceView 는 forward() 를 사용해서 JSP를 실행한다.
    
    > 참고
    >> InternalResourceViewResolver 는 만약 JSTL 라이브러리가 있으면 InternalResourceView 를 상속받은 JstlView 를 반환한다. JstlView 는 JSTL 태그 사용시 약간의 부가 기능이 추가된다.
    > 참고
    >> 다른 뷰는 실제 뷰를 렌더링하지만, JSP의 경우 forward() 통해서 해당 JSP로 이동(실행)해야 렌더링이 된다. JSP를 제외한 나머지 뷰 템플릿들은 forward() 과정 없이 바로 렌더링 된다.
    > 참고
    >>  Thymeleaf 뷰 템플릿을 사용하면 ThymeleafViewResolver 를 등록해야 한다. 최근에는 라이브러리만추가하면 스프링 부트가 이런 작업도 모두 자동화해준다.

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,7 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan//서블릿 자동 등록; 현재 @ServletComponentScan를 추가한 클래스 패키지를 포함한 모든 하위패키지를 색인하여 servlet 을 등록해서 실행할 수 있도록 도와준다.
 @SpringBootApplication
@@ -11,5 +12,19 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	/**
+	 * 뷰 리졸버 - {@link InternalResourceViewResolver}<br>
+	 * 스프링 부트는 {@link InternalResourceViewResolver} 라는 뷰 리졸버를 자동으로 등록하는데,<br>
+	 * 이때 application.properties 에 등록한<br>
+	 * spring.mvc.view.prefix,<br>
+	 * spring.mvc.view.suffix 설정 정보를 사용해서 등록한다.
+	 * <p>
+	 * ; InternalResource?: 내부에서 자원을 호출하는 것(ex. servlet -> jsp 이동)
+	 */
+//	@Bean
+//	ViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 
 }
--- src/test/java/hello/servlet/web/springmvc/old/OldControllerTest.java
+++ src/test/java/hello/servlet/web/springmvc/old/OldControllerTest.java
@@ -1,7 +1,9 @@
 package hello.servlet.web.springmvc.old;
 
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +12,19 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.xml.transform.Source;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,6 +41,12 @@ public class OldControllerTest {
     @Test
     void handleRequestTest() throws Exception {
         ResultActions perform = mvc.perform(get("/springmvc/old-controller"));
-        perform.andDo(print()).andExpect(status().isOk());
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/views/new-form.jsp"));// 다른 뷰는 실제 뷰를 렌더링하지만, JSP의 경우 forward() 통해서 해당 JSP로 이동(실행)해야 렌더링이 된다. JSP를 제외한 나머지 뷰 템플릿들은 forward() 과정 없이 바로 렌더링 된다.
+//                .andExpect(result -> {
+//                    assertThat("/WEB-INF/views/new-form.jsp").isEqualTo(result.getResponse().getForwardedUrl());
+//                });
+
     }
 }
